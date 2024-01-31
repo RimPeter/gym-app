a7a011bc85690a53cff54e5b900d90d048440478
RimPeter <hull@localappliancerentals.co.uk>
2024-01-31 14:43:06 +0000

    Connecting to our API with Python

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('gym_app')
+
+workout = SHEET.worksheet('exercise')
+data = workout.get_all_values()
+print(data)
+
+
+
 
 # Creating a Python dictionary with exercises as keys and muscle types as values
 
