be0a8ea1a19c1778af72464e4f5221d1d4353dbf
RimPeter <hull@localappliancerentals.co.uk>
2024-02-08 10:23:31 +0000

    Add number_of_sets and exerecise_values functions

--- run.py
+++ run.py
@@ -29,22 +29,6 @@ with open('muscledict.json', 'r') as file:
 
 
 '''
-create an app, where you can put the following data in:
-1 exercise
-2 number of sets
-3 number of repetition per each set
-4 the weight for each set
-
-after that the app calculates which muscles has been effected by the exercise regarding:
-1 sum of all weight (sets * reps * weight) by muscle type(column) by each exercise(row)
-2 sum of weight by exercise
-
-also:
-ability to add, remove and modify exercise
-
-'''
-
-'''
 input tree:
 
 1 enter exercise
@@ -65,42 +49,6 @@ input tree:
     5 exit program
 3 exit program
 '''
-
-# muscle_type = input('input muscle type you want to exercise!')
-# muscle_type = muscle_type.lower()
-
-# print(muscle_type)
-
-# print(muscle_dict.keys())
-# for muscle_key in muscle_dict.keys():
-#     if muscle_key == muscle_type:
-#         print(f'you have chosen {muscle_key} type')
-#         for index, exercise in enumerate(muscle_dict[muscle_key], start=1):
-#             print(f'{index}. {exercise}')
-            
-#         user_input = input("Enter the index number to select an exercise: \n")
-#         #print(muscle_dict[muscle_key])
-        
-#         try:
-#             index = int(user_input) - 1
-
-#             # Access and print the exercise at the chosen index
-#             if index >= 0 and index < len(muscle_dict[muscle_key]):
-#                 print(f"The exercise at index {user_input} is: {muscle_dict[muscle_key][index]}")
-#             else:
-#                 print("Invalid index. Please enter a number within the range of the list.")
-#         except ValueError:
-#             # Handle the case where the input is not an integer
-#             print("Please enter a valid integer.")
-#         chosen_exercise = muscle_dict[muscle_key][index]
-
-# number_of_sets = input('enter number of sets!')
-
-# for set_number in range(int(number_of_sets)):
-#     repetition = input('input repetition\n')
-#     weight = input('input weight\n')
-#     print(repetition, weight)
-    
 
 # workout_data = SHEET.worksheet('exercise').get_all_values()
 # print(workout_data)
@@ -153,3 +101,13 @@ def choose_exercise():
 
 print(choose_exercise())
 
+def number_of_sets():
+    number_of_set = input('enter number of sets!')
+    return number_of_set
+
+#number_of_set = number_of_sets()
+def exerecise_values():
+    for set_number in range(int(number_of_sets())):
+        repetition = input('input repetition\n')
+        weight = input('input weight\n')
+        print(repetition, weight)
