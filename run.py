a7a3b675d69597c91ce002e96100ed8d70ef4674
RimPeter <hull@localappliancerentals.co.uk>
2024-02-18 20:26:05 +0000

    Fix errors for PEP8 guidelines

--- run.py
+++ run.py
@@ -47,7 +47,7 @@ def input_muscle_data():
     return muscle_type
 
 
-def choose_exercise():
+def select_exercise():
     '''
     Select exercise based on muscle type; validate choice.
     '''
@@ -62,14 +62,15 @@ def choose_exercise():
                 try:
                     index = int(user_input) - 1
                     if index >= 0 and index < len(muscle_dict[muscle_key]):
-                        chosen_exercise = muscle_dict[muscle_key][index]
-                        print(f"The selected exercise is: {chosen_exercise}")
+                        selected_exercise = muscle_dict[muscle_key][index]
+                        print(f"The selected exercise is: {selected_exercise}")
                         break
                     else:
-                        print("Invalid index. Select within the range of the list.")
+                        print("Invalid index.")
+                        print("Select within the range of the list.")
                 except ValueError:
                     print("Please enter a valid integer.")
-    return chosen_exercise
+    return selected_exercise
 
 
 def number_of_sets():
@@ -87,17 +88,21 @@ def number_of_sets():
             print("Please enter a valid integer.")
 
 
-def update_cumulative(chosen_exercise, total_weight):
+def update_cumulative(selected_exercise, total_weight):
     '''
     Update 'cumulative' sheet with exercise/weight.
     '''
     cumulative_exercises = cumulative.col_values(1)
-    if chosen_exercise not in cumulative_exercises:
-        cumulative.append_row([chosen_exercise, total_weight])
+    if selected_exercise not in cumulative_exercises:
+        cumulative.append_row([selected_exercise, total_weight])
     else:
-        row_number = cumulative_exercises.index(chosen_exercise) + 1
+        row_number = cumulative_exercises.index(selected_exercise) + 1
         current_value = cumulative.cell(row_number, 2).value
-        new_value = int(current_value) + total_weight if current_value.isdigit() else total_weight
+        if current_value.isdigit():
+            new_value = int(current_value) + total_weight
+        else:
+            new_value = total_weight
+
         cumulative.update_cell(row_number, 2, new_value)
 
 
@@ -114,70 +119,82 @@ def get_integer_input(prompt):
                 print("Please enter a number greater than zero.")
         except ValueError:
             print("Invalid input. Please enter a valid integer.")
-            
 
 
 def exercise_values():
     '''
     Record workout data and update 'cumulative' sheet.
     '''
-    chosen_exercise = choose_exercise()
+    selected_exercise = select_exercise()
     total_weight = 0
     sets = get_integer_input('Enter the number of sets:\n')
     for set_number in range(sets):
-        repetition = get_integer_input(f'Input repetition for set {set_number + 1}:\n')
-        weight = get_integer_input(f'Input weight for set {set_number + 1} (in kg):\n')
-        each_set = chosen_exercise, int(set_number+1), int(repetition), int(weight), int(repetition)*int(weight)
+        rep = get_integer_input(
+            f'Input repetition for set {set_number + 1}:\n')
+        weight = get_integer_input(
+            f'Input weight for set {set_number + 1} (in kg):\n')
+        each_set = (
+            selected_exercise,
+            int(set_number+1),
+            int(rep),
+            int(weight),
+            int(rep)*int(weight)
+            )
         workout.append_row(each_set)
-        print(f"Set {set_number + 1}: Repetitions = {repetition}, Weight = {weight}kg")
-        total_weight += repetition * weight
-    update_cumulative(chosen_exercise, total_weight)
+        print(
+            f"Set {set_number + 1}: Repetitions = {rep}, Weight = {weight}kg"
+            )
+        total_weight += rep * weight
+    update_cumulative(selected_exercise, total_weight)
+
 
 def update_exercise_worksheet():
     '''
-    Retrieves and returns the current data from the 'exercise' or 'cumulative' worksheet. 
-    These functions are used to refresh the local representation of worksheet data, 
-    ensuring the script works with the most up-to-date information from the Google Sheet.
+    Retrieves and returns the current data from the 'exercise' or 'cumulative'
+    worksheet. These functions are used to refresh the local representation of
+    worksheet data, ensuring the script works with the most up-to-date
+    information from the Google Sheet.
     '''
     workout = SHEET.worksheet('exercise')
     exercise_data = workout.get_all_values()
     return exercise_data
 
+
 def update_cumulative_worksheet():
     '''
-    Retrieves and returns the current data from the 'exercise' or 'cumulative' worksheet. 
-    These functions are used to refresh the local representation of worksheet data, 
-    ensuring the script works with the most up-to-date information from the Google Sheet.
+    Retrieves and returns the current data from the 'exercise' or 'cumulative'
+    worksheet. These functions are used to refresh the local representation of
+    worksheet data, ensuring the script works with the most up-to-date
+    information from the Google Sheet.
     '''
-
     cumulative = SHEET.worksheet('cumulative')
     cumulative_data = cumulative.get_all_values()
     return cumulative_data
+
 
 def print_my_workout():
     '''
     Print workout data for selected exercise.
     '''
     data = update_exercise_worksheet()
-    # workout = SHEET.worksheet('exercise')
-    # data = workout.get_all_values()
     each_exercise = list(set(exercise[0] for exercise in data[1:]))
     each_exercise.sort()
-    
+
     print("Select an exercise type to print:")
     for index, exercise in enumerate(each_exercise, start=1):
         print(f"{index}. {exercise}")
 
     try:
-        choice_index = int(input('Enter the index number of the workout to print:\n')) - 1
-        chosen_exercise = each_exercise[choice_index]
+        choice_index = int(input(
+            'Enter the index number of the workout to print:\n')) - 1
+        selected_exercise = each_exercise[choice_index]
     except (ValueError, IndexError):
         print("Invalid selection. Please enter a valid index number.")
         return
     print(data[0])
 
     for exercise in data[1:]:
-        if exercise[0] == chosen_exercise:
+        if exercise[0] == selected_exercise:
             print(exercise)
 
 
@@ -190,16 +207,13 @@ def print_sum_of_all_weights():
         print(row)
 
 
-
-    
 def remove_cumulative(rows_to_delete):
     '''
-    This function adjusts the cumulative totals in the 'cumulative' 
-    worksheet by subtracting the weights specified in the rows. 
-    If the new cumulative weight of an exercise drops to zero, 
+    This function adjusts the cumulative totals in the 'cumulative'
+    worksheet by subtracting the weights specified in the rows.
+    If the new cumulative weight of an exercise drops to zero,
     the corresponding row is deleted from the worksheet.
     '''
-
     for row in rows_to_delete:
         exercise_name = row[0]
         weight_to_subtract = int(row[4])
@@ -208,13 +222,11 @@ def remove_cumulative(rows_to_delete):
             row_index = cumulative_exercises.index(exercise_name) + 1
             current_weight = int(cumulative.cell(row_index, 2).value)
             new_weight = max(0, current_weight - weight_to_subtract)
-            
             # If new_weight is zero, delete the row.
             if new_weight == 0:
                 cumulative.delete_rows(row_index)
             else:
                 cumulative.update_cell(row_index, 2, new_weight)
-
 
 
 def delete_recent_rows_of_workout():
@@ -223,10 +235,13 @@ def delete_recent_rows_of_workout():
     '''
     total_rows = len(workout.get_all_values())
     print(f"Current total number of rows: {total_rows}")
-    num_rows_to_delete = int(input("Enter the number of last rows you want to delete: "))
+    num_rows_to_delete = int(input(
+        "Enter the number of last rows you want to delete: "))
 
     if num_rows_to_delete <= 0 or num_rows_to_delete > total_rows:
-        print("Invalid number of rows to delete. Please enter a positive number less than or equal to the total number of rows.")
+        print("Invalid number of rows to delete.")
+        print("Please enter a positive number less than")
+        print("or equal to the total number of rows.")
         return
 
     start_row = total_rows - num_rows_to_delete + 1
